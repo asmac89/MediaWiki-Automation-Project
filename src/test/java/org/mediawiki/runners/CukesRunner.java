bf7b769e81d5643c8737deda92b25f2a078e5fa8
asmac89 <asmachiknawari@hotmail.com>
2018-03-27 17:13:24 -0400

    Initial commit by Asma

---
+++ src/test/java/org/mediawiki/runners/CukesRunner.java
@@ -0,0 +1,17 @@
+package org.mediawiki.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+		plugin= {"pretty",
+				"html:target/cucumber-report"},
+		tags="",
+		features="src/test/resources/com/app/features/",
+		glue="com/app/step_definitions",
+		dryRun=false //by default, it is false
+		)
+
+public class CukesRunner extends AbstractTestNGCucumberTests {
+
+}
---
+++ src/test/java/org/mediawiki/step_definitions/Hooks.java
@@ -0,0 +1,38 @@
+package org.mediawiki.step_definitions;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import org.mediawiki.utilities.Driver;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+public class Hooks {
+	
+	@Before
+	public void setup(Scenario scenario) {
+		WebDriver driver = Driver.getDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
+	}
+
+	@After
+	public void tearDown(Scenario scenario) {
+		if(scenario.isFailed()) {
+			//takes a screenshot
+			final byte[] screenshot = ((TakesScreenshot) 
+			Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+			//adds the screenshots to the report
+			scenario.embed(screenshot, "image/png");
+		}
+		
+		Driver.closeDriver();
+
+	}
+
+}
