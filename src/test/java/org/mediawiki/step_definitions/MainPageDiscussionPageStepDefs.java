11056240fada693c9485009254f96bbfea2ca140
Andylam224 <Andylam224@gmail.com>
2018-04-05 15:29:07 -0400

    added step defs

---
+++ src/test/java/org/mediawiki/step_definitions/MainPageDiscussionPageStepDefs.java
@@ -0,0 +1,5 @@
+package org.mediawiki.step_definitions;
+
+public class MainPageDiscussionPageStepDefs {
+
+}
---
+++ src/test/java/org/mediawiki/step_definitions/MainPageTopicStepDefs.java
@@ -0,0 +1,20 @@
+package org.mediawiki.step_definitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MainPageTopicStepDefs {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
