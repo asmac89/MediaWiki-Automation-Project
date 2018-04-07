857c6f1e956b4be3d98f3e85121826b6a8881eee
asmac89 <asmachiknawari@hotmail.com>
2018-04-07 18:33:37 -0400

    Removed MainPageDiscussionPageStepDefs

--- src/test/java/org/mediawiki/step_definitions/DiscussionEditFunctionalityStepDefs.java
+++ src/test/java/org/mediawiki/step_definitions/DiscussionEditFunctionalityStepDefs.java
@@ -2,7 +2,6 @@ package org.mediawiki.step_definitions;
 
 import static org.testng.Assert.assertTrue;
 
-
 import org.mediawiki.pages.MediaWikiMainPage;
 import org.mediawiki.pages.MediaWikiMainPageDiscussionPage;
 import org.mediawiki.pages.MediaWikiMainPageEditPage;
@@ -13,59 +12,54 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class DiscussionEditFunctionalityStepDefs {
-	MediaWikiMainPage MainPage=new MediaWikiMainPage();
-	MediaWikiMainPageDiscussionPage DiscussionPage=new MediaWikiMainPageDiscussionPage();
-	MediaWikiMainPageEditPage MEditPage=new MediaWikiMainPageEditPage();
+	MediaWikiMainPage MainPage = new MediaWikiMainPage();
+	MediaWikiMainPageDiscussionPage DiscussionPage = new MediaWikiMainPageDiscussionPage();
+	MediaWikiMainPageEditPage MEditPage = new MediaWikiMainPageEditPage();
 
+//	@Then("^Talk:Main Page title should be displayed$")
+//	public void talk_Main_Page_title_should_be_displayed() {
+//		assertTrue(DiscussionPage.isAtDiscussionPage());
+//	}
 
-@Then("^Talk:Main Page title should be displayed$")
-public void talk_Main_Page_title_should_be_displayed() {
-	  assertTrue(DiscussionPage.isAtDiscussionPage());
+//	@When("^I click on the Discussion tab on main page$")
+//	public void i_click_on_the_Discussion_tab_on_main_page() {
+//		MainPage.DiscussionPage.click();
+//	}
+//
+//	@Then("^I click on the Edit tab on discussion page$")
+//	public void i_click_on_the_Edit_tab_on_discussion_page() {
+//		DiscussionPage.Edit.click();
+//	}
+//
+//	@Then("^Editing Talk:Main Page title should be displayed$")
+//	public void editing_Talk_Main_Page_title_should_be_displayed() {
+//		assertTrue(DiscussionPage.isAtDiscussionEditPage());
+//	}
+//
+//	@Then("^\"([^\"]*)\" topic should be displayed on edit page$")
+//	public void topic_should_be_displayed_on_edit_page(String text) {
+//		assertTrue(DiscussionPage.DTopic.getText().equals(text));
+//	}
+//
+//	@Then("^I click on the internal link button on Discussion page$")
+//	public void i_click_on_the_internal_link_button_on_Discussion_page() {
+//		DiscussionPage.internalLink.click();
+//	}
+//
+//	@Then("^\"([^\"]*)\" link should be displayed$")
+//	public void link_should_be_displayed(String Topics) {
+//		assertTrue(DiscussionPage.isLinkDisplayed(Topics).isDisplayed());
+//	}
+//
+//	@Then("^\"([^\"]*)\" is clickable$")
+//	public void is_clickable(String arg) {
+//		assertTrue(DiscussionPage.isClickable(arg));
+//	}
+//
+//	@Then("^I type \"([^\"]*)\" link$")
+//	public void i_type_link(String text) {
+//		MEditPage.MainPageEditInputBox.sendKeys(text + Keys.ARROW_RIGHT + Keys.ARROW_RIGHT + Keys.ENTER + Keys.ENTER);
+//
+//	}
+
 }
-
-@When("^I click on the Discussion tab on main page$")
-public void i_click_on_the_Discussion_tab_on_main_page() {
-    MainPage.DiscussionPage.click();
-}
-@Then("^I click on the Edit tab on discussion page$")
-public void i_click_on_the_Edit_tab_on_discussion_page() {
-    DiscussionPage.Edit.click();
-}
-
-@Then("^Editing Talk:Main Page title should be displayed$")
-public void editing_Talk_Main_Page_title_should_be_displayed() {
-    assertTrue(DiscussionPage.isAtDiscussionEditPage());
-}
-@Then("^\"([^\"]*)\" topic should be displayed on edit page$")
-public void topic_should_be_displayed_on_edit_page(String text) {
-    assertTrue(DiscussionPage.DTopic.getText().equals(text));
-}
-
-@Then("^I click on the internal link button on Discussion page$")
-public void i_click_on_the_internal_link_button_on_Discussion_page() {
-    DiscussionPage.internalLink.click();
-}
-
-@Then("^\"([^\"]*)\" link should be displayed$")
-public void link_should_be_displayed(String Topics) {
-  assertTrue( DiscussionPage.isLinkDisplayed(Topics).isDisplayed());
-}
-
-@Then("^\"([^\"]*)\" is clickable$")
-public void is_clickable(String arg) {
-   assertTrue(DiscussionPage.isClickable(arg));
-}
-@Then("^I type \"([^\"]*)\" link$")
-public void i_type_link(String text) {
-    MEditPage.MainPageEditInputBox.sendKeys(text+Keys.ARROW_RIGHT+Keys.ARROW_RIGHT+Keys.ENTER+Keys.ENTER);
-
-}
-
-
-}
-	
-	
-	
-	
-
-
--- src/test/java/org/mediawiki/step_definitions/MainPageEditFunctionalityStepDefs.java
+++ src/test/java/org/mediawiki/step_definitions/MainPageEditFunctionalityStepDefs.java
@@ -2,7 +2,6 @@ package org.mediawiki.step_definitions;
 
 import static org.testng.Assert.assertTrue;
 
-
 import org.mediawiki.pages.MediaWikiMainPage;
 import org.mediawiki.pages.MediaWikiMainPageDiscussionPage;
 import org.mediawiki.pages.MediaWikiMainPageEditPage;
@@ -13,58 +12,54 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class MainPageEditFunctionalityStepDefs {
-	MediaWikiMainPage MainPage=new MediaWikiMainPage();
-	MediaWikiMainPageEditPage MEditPage=new MediaWikiMainPageEditPage();
-	MediaWikiMainPageDiscussionPage DiscussionPage=new MediaWikiMainPageDiscussionPage();
-	
-	
+	MediaWikiMainPage MainPage = new MediaWikiMainPage();
+	MediaWikiMainPageEditPage MEditPage = new MediaWikiMainPageEditPage();
+	MediaWikiMainPageDiscussionPage DiscussionPage = new MediaWikiMainPageDiscussionPage();
+
 	@Given("^The Main page is displayed$")
 	public void the_Main_page_is_displayed() {
-	assertTrue(	MainPage.isAtMainPage()); 
+		assertTrue(MainPage.isAtMainPage());
 	}
 
-@When("^I click on the Edit tab on main page$")
-public void i_click_on_the_Edit_tab_on_main_page() {
-    MainPage.MainPageEditButton.click();
-}
+	@When("^I click on the Edit tab on main page$")
+	public void i_click_on_the_Edit_tab_on_main_page() {
+		MainPage.MainPageEditButton.click();
+	}
 
-@Then("^Editing Main Page topic title should be displayed$")
-public void editing_Main_Page_topic_title_should_be_displayed() {
-  assertTrue( MEditPage.isAtEditPage());
-}
+	@Then("^Editing Main Page topic title should be displayed$")
+	public void editing_Main_Page_topic_title_should_be_displayed() {
+		assertTrue(MEditPage.isAtEditPage());
+	}
 
-@Then("^I click on the Level (\\d+) Headline text button$")
-public void i_click_on_the_Level_Headline_text_button_for_Main_page(int arg1) {
-  MEditPage.Headline.click();
-}
+	@Then("^I click on the Level (\\d+) Headline text button$")
+	public void i_click_on_the_Level_Headline_text_button_for_Main_page(int arg1) {
+		MEditPage.Headline.click();
+	}
 
-@Then("^I type \"([^\"]*)\"$")
-public void i_type(String text) {
-    MEditPage.MainPageEditInputBox.sendKeys(text);
-}
+	@Then("^I type \"([^\"]*)\"$")
+	public void i_type(String text) {
+		MEditPage.MainPageEditInputBox.sendKeys(text);
+	}
 
-@Then("^I click on save changes button on edit page$")
-public void i_click_on_save_changes_button_on_edit_page() {
-   MEditPage.MainPageEditSaveButton.click();
-}
+	@Then("^I click on save changes button on edit page$")
+	public void i_click_on_save_changes_button_on_edit_page() {
+		MEditPage.MainPageEditSaveButton.click();
+	}
 
-@Then("^\"([^\"]*)\" popup should be displayed$")
-public void popup_should_be_displayed(String arg1) {
-  assertTrue( MainPage.SavedPopup.isDisplayed());
-}
+	@Then("^\"([^\"]*)\" popup should be displayed$")
+	public void popup_should_be_displayed(String arg1) {
+		assertTrue(MainPage.SavedPopup.isDisplayed());
+	}
 
+	@Then("^\"([^\"]*)\" topic should be displayed$")
+	public void topic_should_be_displayed(String text) {
+		assertTrue(MainPage.Topic.getText().equals(text));
 
-@Then("^\"([^\"]*)\" topic should be displayed$")
-public void topic_should_be_displayed(String text) {
-	  assertTrue(MainPage.Topic.getText().equals(text));
+	}
 
-}
-@Then("^\"([^\"]*)\" description should be displayed$")
-public void description_should_be_displayed(String text) {
-  assertTrue(MainPage.Description.getText().contains(text));
-}
-
-
-
+	@Then("^\"([^\"]*)\" description should be displayed$")
+	public void description_should_be_displayed(String text) {
+		assertTrue(MainPage.Description.getText().contains(text));
+	}
 
 }
