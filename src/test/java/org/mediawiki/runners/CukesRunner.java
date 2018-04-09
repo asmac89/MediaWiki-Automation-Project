02fe722578cb213de640e66488fdd658681d865e
asmac89 <asmachiknawari@hotmail.com>
2018-04-09 17:47:01 -0400

    Added json plugin to CukesRunner -Asma

--- src/test/java/org/mediawiki/runners/CukesRunner.java
+++ src/test/java/org/mediawiki/runners/CukesRunner.java
@@ -5,13 +5,10 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
 		plugin= {"pretty",
-				"html:target/cucumber-report"},
-
-
+				"html:target/cucumber-report",
+				"json:target/cucumber.json"
+				},
 		tags="@scenariotest",
-
-
-
 		features="src/test/resources/org/mediawiki/features/",
 		glue="org/mediawiki/step_definitions",
 		dryRun=false //by default, it is false
